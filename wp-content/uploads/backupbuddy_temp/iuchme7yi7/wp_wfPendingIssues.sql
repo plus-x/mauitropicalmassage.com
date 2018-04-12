55eb6193568b1207a69f86a05c058171fac6122b
plus-x <carlos@plu.sx>
2018-04-12 12:00:59 -0500

    auto update

--- wp-content/uploads/backupbuddy_temp/iuchme7yi7/wp_wfPendingIssues.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_wfPendingIssues` (  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,  `time` int(10) unsigned NOT NULL,  `lastUpdated` int(10) unsigned NOT NULL,  `status` varchar(10) NOT NULL,  `type` varchar(20) NOT NULL,  `severity` tinyint(3) unsigned NOT NULL,  `ignoreP` char(32) NOT NULL,  `ignoreC` char(32) NOT NULL,  `shortMsg` varchar(255) NOT NULL,  `longMsg` text,  `data` text,  PRIMARY KEY (`id`),  KEY `lastUpdated` (`lastUpdated`),  KEY `status` (`status`),  KEY `ignoreP` (`ignoreP`),  KEY `ignoreC` (`ignoreC`)) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40000 ALTER TABLE `wp_wfPendingIssues` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_wfPendingIssues` ENABLE KEYS */;
--- wp-content/uploads/backupbuddy_temp/iuchme7yi7/wp_wfReverseCache.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `wp_wfReverseCache` (  `IP` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',  `host` varchar(255) NOT NULL,  `lastUpdate` int(10) unsigned NOT NULL,  PRIMARY KEY (`IP`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40000 ALTER TABLE `wp_wfReverseCache` DISABLE KEYS */;
-INSERT INTO `wp_wfReverseCache` VALUES('\0\0\0\0\0\0\0\0\0\0ÿÿBùI', 'crawl-66-249-73-28.googlebot.com', '1523491765');
-/*!40000 ALTER TABLE `wp_wfReverseCache` ENABLE KEYS */;
