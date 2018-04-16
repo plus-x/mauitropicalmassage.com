d85f4c5f50fb633e42a27919ba74544ed53e1bb9
plus-x <carlos@plu.sx>
2018-04-16 12:01:20 -0500

    auto update

---
+++ wp-content/uploads/backupbuddy_temp/ezwz4su1fk/wp_wfCrawlers.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `wp_wfCrawlers` (  `IP` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',  `patternSig` binary(16) NOT NULL,  `status` char(8) NOT NULL,  `lastUpdate` int(10) unsigned NOT NULL,  `PTR` varchar(255) DEFAULT '',  PRIMARY KEY (`IP`,`patternSig`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40000 ALTER TABLE `wp_wfCrawlers` DISABLE KEYS */;
+INSERT INTO `wp_wfCrawlers` VALUES('\0\0\0\0\0\0\0\0\0\0ÿÿBùAš', '‹üÀ•>b0«oQ1Œö', 'verified', '1523696851', 'crawl-66-249-65-154.googlebot.com');
+INSERT INTO `wp_wfCrawlers` VALUES('\0\0\0\0\0\0\0\0\0\0ÿÿBùAœ', '‹üÀ•>b0«oQ1Œö', 'verified', '1523696852', 'crawl-66-249-65-156.googlebot.com');
+INSERT INTO `wp_wfCrawlers` VALUES('\0\0\0\0\0\0\0\0\0\0ÿÿBùI', '‹üÀ•>b0«oQ1Œö', 'verified', '1523491765', 'crawl-66-249-73-28.googlebot.com');
+INSERT INTO `wp_wfCrawlers` VALUES('\0\0\0\0\0\0\0\0\0\0ÿÿBùI', '‹üÀ•>b0«oQ1Œö', 'verified', '1523222059', 'crawl-66-249-73-27.googlebot.com');
+INSERT INTO `wp_wfCrawlers` VALUES('\0\0\0\0\0\0\0\0\0\0ÿÿBùA˜', '‹üÀ•>b0«oQ1Œö', 'verified', '1523740401', 'crawl-66-249-65-152.googlebot.com');
+INSERT INTO `wp_wfCrawlers` VALUES('\0\0\0\0\0\0\0\0\0\0ÿÿBùA', '‹üÀ•>b0«oQ1Œö', 'verified', '1523696850', 'crawl-66-249-65-127.googlebot.com');
+INSERT INTO `wp_wfCrawlers` VALUES('\0\0\0\0\0\0\0\0\0\0ÿÿBùAe', '‹üÀ•>b0«oQ1Œö', 'verified', '1523696848', 'crawl-66-249-65-101.googlebot.com');
+/*!40000 ALTER TABLE `wp_wfCrawlers` ENABLE KEYS */;
