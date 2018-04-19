0d4123c7c1de868394722c559d3c7cc414e8c63c
plus-x <carlos@plu.sx>
2018-04-19 00:00:56 -0500

    auto update

---
+++ wp-content/uploads/backupbuddy_temp/prfzpkyiqo/wp_wfScanners.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `wp_wfScanners` (  `eMin` int(10) unsigned NOT NULL,  `IP` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',  `hits` smallint(5) unsigned NOT NULL,  PRIMARY KEY (`eMin`,`IP`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40000 ALTER TABLE `wp_wfScanners` DISABLE KEYS */;
+INSERT INTO `wp_wfScanners` VALUES('25400194', '\0\0\0\0\0\0\0\0\0\0ˇˇR—∂\\', '1');
+INSERT INTO `wp_wfScanners` VALUES('25398677', '\0\0\0\0\0\0\0\0\0\0ˇˇ≤ØÅ[', '1');
+INSERT INTO `wp_wfScanners` VALUES('25399088', '\0\0\0\0\0\0\0\0\0\0ˇˇ_µ≥?', '23');
+INSERT INTO `wp_wfScanners` VALUES('25399628', '\0\0\0\0\0\0\0\0\0\0ˇˇºäi?', '1');
+INSERT INTO `wp_wfScanners` VALUES('25398506', '\0\0\0\0\0\0\0\0\0\0ˇˇºäi?', '1');
+INSERT INTO `wp_wfScanners` VALUES('25399107', '\0\0\0\0\0\0\0\0\0\0ˇˇ_µ≥?', '1');
+/*!40000 ALTER TABLE `wp_wfScanners` ENABLE KEYS */;
