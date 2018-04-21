00aad0b101e209aa752c69142f9624d4d1e7f4c2
plus-x <carlos@plu.sx>
2018-04-21 12:00:54 -0500

    auto update

--- wp-content/uploads/backupbuddy_temp/9s7ysg8m41/wp_wfScanners.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `wp_wfScanners` (  `eMin` int(10) unsigned NOT NULL,  `IP` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',  `hits` smallint(5) unsigned NOT NULL,  PRIMARY KEY (`eMin`,`IP`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40000 ALTER TABLE `wp_wfScanners` DISABLE KEYS */;
-INSERT INTO `wp_wfScanners` VALUES('25403708', '\0\0\0\0\0\0\0\0\0\0ÿÿÌÓ8', '4');
-INSERT INTO `wp_wfScanners` VALUES('25402875', '\0\0\0\0\0\0\0\0\0\0ÿÿÁF\"Ñ', '1');
-INSERT INTO `wp_wfScanners` VALUES('25402737', '\0\0\0\0\0\0\0\0\0\0ÿÿ¼Ši', '1');
-/*!40000 ALTER TABLE `wp_wfScanners` ENABLE KEYS */;
--- wp-content/uploads/backupbuddy_temp/9s7ysg8m41/wp_wfVulnScanners.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_wfVulnScanners` (  `IP` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',  `ctime` int(10) unsigned NOT NULL,  `hits` int(10) unsigned NOT NULL,  PRIMARY KEY (`IP`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40000 ALTER TABLE `wp_wfVulnScanners` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_wfVulnScanners` ENABLE KEYS */;
