63c84a83d0627629e63bef6cefb65e885e65b0ab
plus-x <carlos@plu.sx>
2018-04-27 00:01:00 -0500

    auto update

---
+++ wp-content/uploads/backupbuddy_temp/614nywrucz/wp_wfReverseCache.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `wp_wfReverseCache` (  `IP` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',  `host` varchar(255) NOT NULL,  `lastUpdate` int(10) unsigned NOT NULL,  PRIMARY KEY (`IP`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40000 ALTER TABLE `wp_wfReverseCache` DISABLE KEYS */;
+INSERT INTO `wp_wfReverseCache` VALUES('\0\0\0\0\0\0\0\0\0\0ÿÿBùI\Z', 'crawl-66-249-73-26.googlebot.com', '1524676929');
+/*!40000 ALTER TABLE `wp_wfReverseCache` ENABLE KEYS */;
