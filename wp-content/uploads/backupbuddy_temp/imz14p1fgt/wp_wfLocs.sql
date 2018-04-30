25265ec74a2d48633272adcfd8c43a1b83d7395e
plus-x <carlos@plu.sx>
2018-04-30 12:01:01 -0500

    auto update

--- wp-content/uploads/backupbuddy_temp/imz14p1fgt/wp_wfLocs.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_wfLocs` (  `IP` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',  `ctime` int(10) unsigned NOT NULL,  `failed` tinyint(3) unsigned NOT NULL,  `city` varchar(255) DEFAULT '',  `region` varchar(255) DEFAULT '',  `countryName` varchar(255) DEFAULT '',  `countryCode` char(2) DEFAULT '',  `lat` float(10,7) DEFAULT '0.0000000',  `lon` float(10,7) DEFAULT '0.0000000',  PRIMARY KEY (`IP`)) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40000 ALTER TABLE `wp_wfLocs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_wfLocs` ENABLE KEYS */;
--- wp-content/uploads/backupbuddy_temp/imz14p1fgt/wp_wfReverseCache.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_wfReverseCache` (  `IP` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',  `host` varchar(255) NOT NULL,  `lastUpdate` int(10) unsigned NOT NULL,  PRIMARY KEY (`IP`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40000 ALTER TABLE `wp_wfReverseCache` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_wfReverseCache` ENABLE KEYS */;
