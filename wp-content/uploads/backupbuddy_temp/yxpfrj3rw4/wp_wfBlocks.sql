c425ad8b3c854ac4c6ebefdf00533923350c07be
plus-x <carlos@plu.sx>
2018-05-01 12:00:55 -0500

    auto update

--- wp-content/uploads/backupbuddy_temp/yxpfrj3rw4/wp_wfBlocks.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_wfBlocks` (  `IP` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',  `blockedTime` bigint(20) NOT NULL,  `reason` varchar(255) NOT NULL,  `lastAttempt` int(10) unsigned DEFAULT '0',  `blockedHits` int(10) unsigned DEFAULT '0',  `wfsn` tinyint(3) unsigned DEFAULT '0',  `permanent` tinyint(3) unsigned DEFAULT '0',  PRIMARY KEY (`IP`),  KEY `k1` (`wfsn`)) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40000 ALTER TABLE `wp_wfBlocks` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_wfBlocks` ENABLE KEYS */;
--- wp-content/uploads/backupbuddy_temp/yxpfrj3rw4/wp_wfReverseCache.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `wp_wfReverseCache` (  `IP` binary(16) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',  `host` varchar(255) NOT NULL,  `lastUpdate` int(10) unsigned NOT NULL,  PRIMARY KEY (`IP`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40000 ALTER TABLE `wp_wfReverseCache` DISABLE KEYS */;
-INSERT INTO `wp_wfReverseCache` VALUES('\0\0\0\0\0\0\0\0\0\0ÿÿBùAT', 'crawl-66-249-65-84.googlebot.com', '1525136599');
-/*!40000 ALTER TABLE `wp_wfReverseCache` ENABLE KEYS */;
